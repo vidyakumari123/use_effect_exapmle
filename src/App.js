169d36205cfc6c957fafafe50c4381ec1699f4fb
vidyakumari123 <127233800+vidyakumari123@users.noreply.github.com>
2024-03-12 17:51:30 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Example1 from './Example1';
 import Example2 from './Example2';
+import UseEffect from './UseEffect'
+// import ClassLifecycle from './ClassLifecycle'
 
 function App() {
+  const [flag, setFlag] = useState(true)
   return (
   <>
-
+  {/* <button onClick={() => setFlag(false)}>Change the Flag</button>
+      {flag ? <ClassLifecycle color="green" /> : <h1>Component Removed</h1>} */}
+      {/* <UseEffect /> */}
   {/* <Example1/> */}
-  <Example2/>
+  {/* <Example2/> */}
   </>
   );
 }
